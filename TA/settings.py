66aa6627bcc477c977d823f82ca4b7d4050bd8a5
Rafifanwar <117049583+Rafifanwar@users.noreply.github.com>
2025-05-05 17:29:33 +0800

    Update settings.py

--- TA/settings.py
+++ TA/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-r%&j@%+5)jpzty^wtew%g09jh@-#f!g0b@$u*prsa06i6f!a=s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.vercel.app','127.0.0.1','.now.sh','.railway.app']
 
